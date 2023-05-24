a12660d4b3fbc6e02bf5fd1e8db1f4eef1d48be5
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-05-24 15:48:07 -0500

    add mac path

--- dockerstats.go
+++ dockerstats.go
@@ -3,35 +3,38 @@
 //
 // To get the statistics of running Docker containers, you can use the `Current()` function:
 //
-// 		stats, err := dockerstats.Current()
-//		if err != nil {
-//			panic(err)
-//		}
+//	stats, err := dockerstats.Current()
+//	if err != nil {
+//		panic(err)
+//	}
 //
-//		for _, s := range stats {
-//			fmt.Println(s.Container) // 9f2656020722
-//			fmt.Println(s.Memory) // {Raw=221.7 MiB / 7.787 GiB, Percent=2.78%}
-//			fmt.Println(s.CPU) // 99.79%
-//		}
+//	for _, s := range stats {
+//		fmt.Println(s.Container) // 9f2656020722
+//		fmt.Println(s.Memory) // {Raw=221.7 MiB / 7.787 GiB, Percent=2.78%}
+//		fmt.Println(s.CPU) // 99.79%
+//	}
 //
 // Alternatively, you can use the `NewMonitor()` function to receive a constant stream of Docker container stats,
 // available on the Monitor's `Stream` channel:
 //
-// 		m := dockerstats.NewMonitor()
+//	m := dockerstats.NewMonitor()
 //
-// 		for res := range m.Stream {
-//			if res.Error != nil {
-//				panic(err)
-//			}
+//	for res := range m.Stream {
+//		if res.Error != nil {
+//			panic(err)
+//		}
 //
-//			for _, s := range res.Stats {
-//				fmt.Println(s.Container) // 9f2656020722
-//			}
-// 		}
+//		for _, s := range res.Stats {
+//			fmt.Println(s.Container) // 9f2656020722
+//		}
+//	}
 package dockerstats
+
+import "runtime"
 
 const (
 	defaultDockerPath        string = "/usr/bin/docker"
+	macOSDockerPath          string = "usr/local/bin/docker"
 	defaultDockerCommand     string = "stats"
 	defaultDockerNoStreamArg string = "--no-stream"
 	defaultDockerFormatArg   string = "--format"
@@ -48,11 +51,19 @@ var DefaultCommunicator Communicator = CliCommunicator{
 	Command:    []string{defaultDockerCommand, defaultDockerNoStreamArg, defaultDockerFormatArg, defaultDockerFormat},
 }
 
+var MacOSCommunicator Communicator = CliCommunicator{
+    DockerPath: macOSDockerPath,
+	Command:    []string{defaultDockerCommand, defaultDockerNoStreamArg, defaultDockerFormatArg, defaultDockerFormat},
+}
+
 // Current returns the current `Stats` of each running Docker container.
 //
 // Current will always return a `[]Stats` slice equal in length to the number of
 // running Docker containers, or an `error`. No error is returned if there are no
 // running Docker containers, simply an empty slice.
 func Current() ([]Stats, error) {
+    if runtime.GOOS == "darwin" {
+        return MacOSCommunicator.Stats()
+    }
 	return DefaultCommunicator.Stats()
 }
